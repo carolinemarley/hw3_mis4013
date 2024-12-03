97e3f96bbf4bd50d607716d52d62f834be503e68
carolinemarley <143441565+carolinemarley@users.noreply.github.com>
2024-12-02 19:49:33 -0600

    Create books.php

---
+++ books.php
@@ -0,0 +1,10 @@
+<?php 
+require_once("util-db.php"); 
+require_once("model-books.php");
+
+$pageTitle = "Books";
+include "view-header.php";
+$books = selectBooks(); 
+include "view-books.php";
+include "view-footer.php";
+?>
