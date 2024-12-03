58a9dfdf082697d5e62b006e4893ca581594bba0
carolinemarley <143441565+carolinemarley@users.noreply.github.com>
2024-12-02 18:19:35 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -1,5 +1,3 @@
-
-
 <!doctype html>
 <html lang="en">
   <head>
@@ -22,17 +20,14 @@
           <a class="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="post.php">Post</a>
+          <a class="nav-link" href="authors.php">Authors</a>
         </li>
          <li class="nav-item">
-          <a class="nav-link" href="post-result.php">Post Result</a>
+          <a class="nav-link" href="books.php">Books</a>
         </li>
          <li class="nav-item">
-          <a class="nav-link" href="get.php">Get</a>
-        </li>
-         <li class="nav-item">
-          <a class="nav-link" href="get-result.php">Get Result</a>
-        </li>
+          <a class="nav-link" href="authors-with-books.php">Authors with Books</a>
+         </li>
     </div>
   </div>
 </nav>
