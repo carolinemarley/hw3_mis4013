867d6fd16c2b97f2e8a608246190497dd98fa9ba
carolinemarley <143441565+carolinemarley@users.noreply.github.com>
2024-12-04 08:30:25 -0600

    Update model-books.php

--- model-books.php
+++ model-books.php
@@ -12,4 +12,48 @@ function selectBooks() {
     throw $e;
   }
 }
+
+function insertBook($bName, $bPublisher) {
+  try {
+    $conn = get_db_connection();
+    $stmt = $conn->prepare("INSERT INTO `book` (`book_title`, `book_publisher`) VALUES (?, ?)");
+    $stmt->bind_param("ss", $bName, $bPublisher);
+    $success = $stmt->execute(); 
+    $conn->close(); 
+    return $success; 
+  } catch (Exception $e){
+      $conn->close();
+    throw $e;
+  }
+}
+
+function updateBook($bName, $bPublisher, $bid) {
+  try {
+    $conn = get_db_connection();
+    $stmt = $conn->prepare("INSERT INTO `book` (`book_title`, `book_publisher`) VALUES (?, ?)");
+    $stmt->bind_param("ss", $bName, $bPublisher);
+    $success = $stmt->execute(); 
+    $conn->close(); 
+    return $success; 
+  } catch (Exception $e){
+      $conn->close();
+    throw $e;
+  }
+}
+
+function deleteBook($bid) {
+  try {
+    $conn = get_db_connection();
+    $stmt = $conn->prepare("delete from book where book_id =?");
+    $stmt->bind_param("i", $bid);
+    $success = $stmt->execute(); 
+    $conn->close(); 
+    return $success; 
+  } catch (Exception $e){
+      $conn->close();
+    throw $e;
+  }
+}
 ?>
+
+
