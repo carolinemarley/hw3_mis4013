a4054cddf449d0645779832a87048fb240a77aca
carolinemarley <143441565+carolinemarley@users.noreply.github.com>
2024-12-04 00:12:05 -0600

    Update model-authors-with-books.php

--- model-authors-with-books.php
+++ model-authors-with-books.php
@@ -12,4 +12,20 @@ function selectAuthors() {
         throw $e;
     }
 }
+
+function selectBooksByAuthor($aid) {
+  try {
+    $conn = get_db_connection();
+    $stmt = $conn->prepare("SELECT b.book_id, book_publisher, book_title, age_group, category_code, category_name FROM book b join category c on c.book_id where c.book_id =?");
+    $stmt->bind_param("i", $aid);
+    $stmt->execute(); 
+    $result = $stmt->get_result();
+    $conn->close(); 
+    return $result; 
+  } catch (Exception $e){
+      $conn->close();
+    throw $e;
+  }
+}
+?>
 ?>
