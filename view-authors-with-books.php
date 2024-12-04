7d688926bc93fdd2b207ef05b31b644377723d3c
carolinemarley <143441565+carolinemarley@users.noreply.github.com>
2024-12-04 00:27:44 -0600

    Update view-authors-with-books.php

--- view-authors-with-books.php
+++ view-authors-with-books.php
@@ -7,13 +7,16 @@ while ($author = $authors->fetch_assoc()) {
     <div class="card-body">
       <h5 class="card-title"><?php echo $author['author_name'];?></h5>
       <p class="card-text">
+      <ul class="list-group">
   <?php
     $books = selectBooksByAuthor($author['author_id']);
   while ($book = $books->fetch_assoc()){
 ?>
+     <li class="list-group-item"><?php echo $book['book_title']?> - <?php echo $book['age_group']?> - <?php echo $book['category_code']?> - <?php echo $book['category_name']?> </li>
 <?php
   }
   ?>
+      </ul>
       </p>
       <p class="card-text"><small class="text-body-secondary">Genre: <?php echo $author['genre'];?></small></p>
     </div>
