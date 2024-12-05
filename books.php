c97224765bc0dbb8d265bcfed4ffb01ac4e02aae
carolinemarley <143441565+carolinemarley@users.noreply.github.com>
2024-12-04 22:21:44 -0600

    Update books.php

--- books.php
+++ books.php
@@ -4,6 +4,14 @@ require_once("model-books.php");
 
 $pageTitle = "Books";
 include "view-header.php";
+
+if (isset($_POST['actionType']) {
+    switch ($_POST['actionType']) {
+    case "Add":
+      insertBook($_POST['bTitle'], $_POST['bPublisher']);
+      break;
+    }
+    }
 $books = selectBooks(); 
 include "view-books.php";
 include "view-footer.php";
