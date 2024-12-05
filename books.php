33ae15726ce6df969f5a92c6f24ad0fcb3c27492
carolinemarley <143441565+carolinemarley@users.noreply.github.com>
2024-12-04 22:27:57 -0600

    Update books.php

--- books.php
+++ books.php
@@ -8,7 +8,11 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
     switch ($_POST['actionType']) {
     case "Add":
-      insertBook($_POST['bTitle'], $_POST['bPublisher']);
+      if (insertBook($_POST['bTitle'], $_POST['bPublisher'])) {
+          echo '<div class="alert alert-success" role="alert">Book Added!</div>';
+      } else {
+           echo '<div class="alert alert-danger" role="alert">ERROR!</div>';
+      }
       break;
     }
     }
