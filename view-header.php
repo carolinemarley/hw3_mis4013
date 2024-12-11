3b871764d9df6caa38d823c87aca2c0dbe1406d2
carolinemarley <143441565+carolinemarley@users.noreply.github.com>
2024-12-10 22:23:50 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -28,6 +28,9 @@
          <li class="nav-item">
           <a class="nav-link" href="authors-with-books.php">Authors with Books</a>
          </li>
+         <li class="nav-item">
+          <a class="nav-link" href="authors-chart.php">Authors Chart</a>
+         </li>
     </div>
   </div>
 </nav>
