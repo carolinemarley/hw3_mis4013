305dff4c6848d51ab3254ebc64b865334c7e2bd4
carolinemarley <143441565+carolinemarley@users.noreply.github.com>
2024-12-10 23:06:48 -0600

    Update authors-chart.php

--- authors-chart.php
+++ authors-chart.php
@@ -1,10 +1,18 @@
 <?php
 require_once("util-db.php");
-require_once("model-authors-chart.php"); 
+require_once("model-authors-chart.php");
 
 $pageTitle = "Authors Chart";
-include "view-header.php";
+
+// Fetch authors data before including the header or any views
 $authors = selectAuthors();
+
+// Now, include the header, which can safely access the $authors variable
+include "view-header.php";
+
+// Include the page content
 include "page.php";
+
+// Include the footer
 include "view-footer.php";
 ?>
