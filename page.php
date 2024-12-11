61b99ed4fd6735d87ed54729af4480873b4c3956
carolinemarley <143441565+carolinemarley@users.noreply.github.com>
2024-12-10 22:51:09 -0600

    Update page.php

--- page.php
+++ page.php
@@ -1,8 +1,36 @@
 <h1>Authors Chart</h1>
+<div>
+  <canvas id="myChart"></canvas>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
+
+
+<script>
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+    datasets: [{
+        data: [
 <?php
 while ($author = $authors->fetch_assoc()) {
-  
+  echo $author['num_categories'] . "', ";
 }
-?>
-   
+?> 
+        ]
+    }],
+
+    // These labels appear in the legend and in the tooltips when hovering different arcs
+    labels: [
+<?php
+$authors = selectAuthors(); 
+while ($author = $authors->fetch_assoc()) {
+  echo "'" .$author['author_name'] . ", ";
+}
+?> 
+    ]
+},
+  });
+</script>
